d70ed99f7eb6cb2ce15eee1c88bec003d9a72f94
Java Software <pj.midgard@yahoo.com>
2021-09-27 16:33:23 +0100

    Add files via upload

--- White_hole_2026.py
+++ White_hole_2026.py
@@ -420,7 +420,7 @@ class compression:
                                     
                                     if i==1:
                                                 Spin=0
-                                                sda3=sda2[6000:]
+                                                sda3=sda2[:6000]
                                                 ei4=0
                                                 ei5=131
                                                 block3=0
