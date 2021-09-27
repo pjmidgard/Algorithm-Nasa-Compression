fe7e11a8ec157fb43ac7f288647ec852de092952
Java Software <pj.midgard@yahoo.com>
2021-09-27 20:59:26 +0100

    Update White_hole_2026.py

--- White_hole_2026.py
+++ White_hole_2026.py
@@ -1005,7 +1005,7 @@ class compression:
                                                     assxq=assxq+1
                                                     
                                                     
-                                                    if assxq==2000:
+                                                    if assxq==20000:
                                                         assx=10 
                                                     if assx==10:        
                                               
